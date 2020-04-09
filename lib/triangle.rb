aa97871fe09f920f1d1827d12c9bc7f9e60cedbe
CiaraMaria Proia <proiacm@gmail.com>
2020-04-09 00:29:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
-  # write code here
+  
+  def initialize(side1, side2, side3)
+    @triangles = []
+    @triangles << side1
+    @triangles << side2
+    @triangles << side3
+  end
+
+  def kind
+    if @triangles[0] == @triangles[1] && @triangles[1] == @triangles[2] && @triangles[0] > 0 
+        return :equilateral
+      elsif @triangles.uniq.length == 2 && 
+        return :isosceles
+      else
+        :scalene
+    end
+  end
+  
+  class TriangleError < StandardError 
+    
+  end
 end
