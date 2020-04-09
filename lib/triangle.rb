e3369d59cde823d223bb6a06d61bd2d95a27f89d
CiaraMaria Proia <proiacm@gmail.com>
2020-04-09 00:34:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,6 +6,10 @@ class Triangle
     @triangles << side2
     @triangles << side3
   end
+  
+  def valid?
+    
+  end
 
   def kind
     if @triangles[0] == @triangles[1] && @triangles[1] == @triangles[2] && @triangles[0] > 0 
